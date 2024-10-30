aa5016b07613431d9bd598479cdb0d78b283069c
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-30 10:18:39 +0700

    Develop POST endpoint for Create Product and implement global using for Carter Mapster and MediatR

--- src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace Catalog.API.Products.CreaateProduct;
+﻿namespace Catalog.API.Products.CreaateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
@@ -10,6 +8,22 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapPost("/products", 
+            async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = request.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
         throw new NotImplementedException();
     }
 }
