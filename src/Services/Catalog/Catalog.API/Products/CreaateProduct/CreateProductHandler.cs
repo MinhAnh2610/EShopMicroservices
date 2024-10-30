6be6f8b57686e5e6e0ccea05f02ac388f3f7c71d
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-30 11:26:51 +0700

    Install Marten to save Product to DB

--- src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductHandler.cs
@@ -1,12 +1,9 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-
-namespace Catalog.API.Products.CreaateProduct;
+﻿namespace Catalog.API.Products.CreaateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -23,8 +20,11 @@ internal class CreateProductCommandHandler : ICommandHandler<CreateProductComman
 
         // TODO Save to database
 
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
         // Return CreateProductResult result
 
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
