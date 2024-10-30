729000b9677ef75489548565228670d9e010be41
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-30 10:08:07 +0700

    Install Mapster package for object mapping

--- src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductEndpoint.cs
@@ -1,9 +1,15 @@
-﻿namespace Catalog.API.Products.CreaateProduct;
+﻿using Carter;
+
+namespace Catalog.API.Products.CreaateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
 public record CreateProductResponse(Guid Id);
 
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
 }
