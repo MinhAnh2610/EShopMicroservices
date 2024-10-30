942964d8e97f070423d0c9abd945a257f7640d26
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-30 09:42:59 +0700

    Create abstraction on building blocks in the Catalog.API

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TComamnd> : ICommandHandler<TComamnd, Unit>
+    where TComamnd : ICommand<Unit>
+{ 
+
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> 
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+
+}
