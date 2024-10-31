1c8f2c1c866f64ef523c048525fc7a0b3b940758
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-10-31 10:49:05 +0700

    Add Validator to Create product handler

--- src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreaateProduct/CreateProductHandler.cs
@@ -3,13 +3,28 @@
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) 
+public class CreateProuctCommandValidator : AbstractValidator<CreateProductCommand>
+{
+  public CreateProuctCommandValidator()
+  {
+    RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+    RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+    RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+    RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greated than 0");
+  }
+}
+
+internal class CreateProductCommandHandler(IDocumentSession session, IValidator<CreateProductCommand> validator, ILogger<CreateProductCommandHandler> logger)
   : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
   public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
   {
     logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
     // Create Product entity from command object
+
+    var result = await validator.ValidateAsync(command, cancellationToken);
+    var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+    if (errors.Any()) throw new ValidationException(errors.FirstOrDefault());
 
     var product = new Product
     {
