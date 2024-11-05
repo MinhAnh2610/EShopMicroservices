53c36e6e65008f60656e386cb8937b0f8c9f8866
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-11-05 10:56:39 +0700

    Configure Basket.API as Client for the Discount.Grpc

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -18,10 +18,9 @@ public class StoreBasketCommandHandler(IBasketRepository repository)
 {
   public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
   {
-    ShoppingCart cart = command.Cart;
+    // TODO : communicate with Discount.grpc
 
     await repository.StoreBasket(command.Cart, cancellationToken);
-    //TODO: update cache
 
     return new StoreBasketResult(command.Cart.UserName);
   }
