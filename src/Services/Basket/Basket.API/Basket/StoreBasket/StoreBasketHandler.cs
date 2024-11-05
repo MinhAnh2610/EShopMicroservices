52d981d4c5586c535589ce73caa3d5e320c19732
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-11-05 11:07:04 +0700

    Consume GetDiscount grpc

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 
@@ -13,15 +15,25 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
   }
 }
 
-public class StoreBasketCommandHandler(IBasketRepository repository)
+public class StoreBasketCommandHandler
+  (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
   : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
   public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
   {
-    // TODO : communicate with Discount.grpc
+    await DeductDiscount(command.Cart, cancellationToken);
 
     await repository.StoreBasket(command.Cart, cancellationToken);
 
     return new StoreBasketResult(command.Cart.UserName);
+  }
+
+  public async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+  {
+    foreach (var item in cart.Items)
+    {
+      var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName });
+      item.Price -= coupon.Amount;
+    }
   }
 }
