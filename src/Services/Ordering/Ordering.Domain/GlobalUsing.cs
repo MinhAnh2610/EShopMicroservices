57cc17052ef50f42ac6e414dbc76f62b9d575513
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-11-06 14:27:46 +0700

    Develop rich-domain entity  models

--- src/Services/Ordering/Ordering.Domain/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Domain/GlobalUsing.cs
@@ -1,3 +1,5 @@
 ﻿global using Ordering.Domain.Abstraction;
 global using Ordering.Domain.Enums;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Exceptions;
+global using Ordering.Domain.Models;
