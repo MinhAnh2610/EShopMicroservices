7f4341ff6d0af7f29fca576ab313ba0e73419a73
Phạm Hoàng Minh Anh <anhphmse183496@fpt.edu.vn>
2024-11-07 13:24:52 +0700

    Seed Products and Orders

--- src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -18,6 +18,8 @@ public static class DatabaseExtensions
   private static async Task SeedAsync(ApplicationDbContext context)
   {
     await SeedCustomerAsync(context);
+    await SeedProductAsync(context);
+    await SeedOrdersWithItemsAsync(context);
   }
 
   private static async Task SeedCustomerAsync(ApplicationDbContext context)
@@ -25,6 +27,24 @@ public static class DatabaseExtensions
     if (!await context.Customers.AnyAsync())
     {
       await context.Customers.AddRangeAsync(InitialData.Customers);
+      await context.SaveChangesAsync();
+    }
+  }
+
+  private static async Task SeedProductAsync(ApplicationDbContext context)
+  {
+    if (!await context.Products.AnyAsync())
+    {
+      await context.Products.AddRangeAsync(InitialData.Products);
+      await context.SaveChangesAsync();
+    }
+  }
+
+  private static async Task SeedOrdersWithItemsAsync(ApplicationDbContext context)
+  {
+    if (!await context.Orders.AnyAsync())
+    {
+      await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
       await context.SaveChangesAsync();
     }
   }
