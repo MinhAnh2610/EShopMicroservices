e628cb45d24c011f37127d0adeb9843dbc57b0f4
Pham Hoang Minh Anh <minhanh26102004@gmail.com>
2024-11-30 15:54:56 +0700

    GetProducts for Index page

--- src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -4,10 +4,10 @@ public class ProductModel
 {
   public Guid Id { get; set; }
   public string Name { get; set; } = default!;
-  public List<string> Category { get; set; } = default!;
+  public List<string> Category { get; set; } = new();
   public string Description { get; set; } = default!;
   public string ImageFile { get; set; } = default!;
-  public string Price { get; set; } = default!;
+  public decimal Price { get; set; }
 }
 
 // wrapper classes
