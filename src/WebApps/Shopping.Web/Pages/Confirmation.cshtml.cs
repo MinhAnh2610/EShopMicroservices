2df04102a7c5eebe006072b4bfdf28b2727e5299
Pham Hoang Minh Anh <minhanh26102004@gmail.com>
2024-12-01 19:48:13 +0700

    Checkout and Confirmation cshtml

--- src/WebApps/Shopping.Web/Pages/Confirmation.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Confirmation.cshtml.cs
@@ -3,10 +3,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Shopping.Web.Pages
 {
-    public class ConfirmationModel : PageModel
+  public class ConfirmationModel : PageModel
+  {
+    public string Message { get; set; } = default!;
+
+    public void OnGetContact()
     {
-        public void OnGet()
-        {
-        }
+      Message = "Your email was sent.";
     }
+
+    public void OnGetOrderSubmitted()
+    {
+      Message = "Your order submitted successfully.";
+    }
+  }
 }
